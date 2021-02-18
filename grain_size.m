fc3b5625d12ebce203b9f9c8f85c13abf98884b1
Aaron Bakx <a.h.j.bakx@student.tudelft.nl>
2021-02-18 09:57:32 +0100

    Polarimetric anomaly

--- grain_size.m
+++ grain_size.m
@@ -11,6 +11,8 @@ fits_albedo_r = load_fits("Ar");
 fits_pmax_v = load_fits("Pv");
 fits_pmax_r = load_fits("Pr");
 
+albedo_b = log10(100*load_fits("Ab"));
+pmax_b = log10(1000*load_fits("Pb"));
 
 %% Interpolate to 650 nm
 
@@ -18,15 +20,6 @@ fits_pmax_r = load_fits("Pr");
 median_albedo_v = median(median(fits_albedo_v, 'omitnan'), 'omitnan');
 median_albedo_r = median(median(fits_albedo_r, 'omitnan'), 'omitnan');
 albedo_650 = log10( 100 * (fits_albedo_v + (median_albedo_r - median_albedo_v) / (676.3 - 558.6) * (650 - 558.6)) );
-% size_albedo = size(fits_albedo_v);
-% albedo_650 = zeros(size_albedo);
-% for i = 1:size_albedo(1)
-%     for j = 1:size_albedo(2)
-%         slope = (fits_albedo_r(i,j) - fits_albedo_v(i,j) ) / (676.3 - 558.6);
-%         albedo_650(i,j) = log10( 100 * (fits_albedo_v(i,j) + slope * (650 - 558.6)) );
-%     end
-% end
-
 
 % interpolate pmax to 650 nm with units log(permilli)
 size_pmax = size(fits_pmax_v);
@@ -43,17 +36,50 @@ end
 % plot_map(albedo_650, ' $A$', latitude, longitude, true, 'Albedo650', true)
 %plot_map(pmax_650, ' $A$', latitude, longitude, true, 'Albedo650', true)
 
-%% Calculate grain size
+%% Calculate grain size for 650 nm
 
 a = 0.845;
 
 size_pixels = size(latitude);
 grain_size_SO92 = zeros(size_pixels);
+polarimetric_anomaly = zeros(size_pixels);
 for i = 1:size_pixels(1)
     for j = 1:size_pixels(2)
-        grain_size_SO92(i,j) = log10(0.03 * exp( 2.9 * ( albedo_650(i,j) + a * pmax_650(i,j) ) ) );
+        polarimetric_anomaly(i,j) = 10^(pmax_b(i,j)^a)*10^albedo_b(i,j);
+        grain_size_SO92(i,j) = 0.03 * exp( 2.9 * ( albedo_650(i,j) + a * pmax_650(i,j) ) );
     end
 end
+
+%% Plot polarimetric anomaly
+close all;
+figure('Position', [500 500 900 900])
+axesm vperspec
+geoshow(latitude, longitude, polarimetric_anomaly, 'DisplayType','texturemap')
+
+colormap gray
+% cbar = colorbar('southoutside');
+
+% mid = (cbar.Limits(1) + cbar.Limits(2))/2;
+%set(cbar, 'Ticks', round([cbar.Limits(1), mid, cbar.Limits(2)], 2))
+%set(cbar, 'Ticks', round([1.80, 1.93, 2.06], 2))
+%caxis(round(cbar.Limits, 2))
+% xlabel(cbar, '$b=(P_{\textrm{max}})^aA$','Interpreter','latex' )
+caxis([300 800])
+% Adjust width of colorbar
+% x1=get(gca,'position');
+% x=get(cbar,'Position');
+% x(1) = x(1) + 0.1250;
+% x(3) = 0.5;
+% set(cbar,'Position',x)
+% set(gca,'position',x1)
+
+% plotm(0.6875, 23.4333, 'wd', 'MarkerSize', 20, 'LineWidth', 5)
+% geoshow('/Users/aaron/thesis/Data/mare_shape/LROC_GLOBAL_MARE_180.shp', 'DisplayType','polygon','FaceColor','none','EdgeColor','k', 'LineWidth', 0.5);
+
+axis off
+set(gca, 'FontSize', 20)
+
+% exportgraphics(gca,'Figures/WP3/polarimetric_anomaly.png','Resolution', 300)
 
 %% Plot grain size
 
@@ -85,7 +111,7 @@ set(gca,'position',x1)
 axis off
 set(gca, 'FontSize', 20)
 
-exportgraphics(gca,'Figures/WP3/grain_size_crameri.png','Resolution', 300)
+% exportgraphics(gca,'Figures/WP3/grain_size_crameri.png','Resolution', 300)
 
 %% Visualise Umov's law in 650 nm images
 
@@ -102,44 +128,86 @@ pmax_highlands = pmax_650(highlands_mask);
 % pmax_highlands = pmax_highlands(1:1000,:);
 
 logA = linspace(0.7, 1.6, 10);
-logPmax = log10( (10.^((1.81 - logA)/0.724))*10); % convert to percent
+logPmax = log10( (10.^((1.81 - logA)/0.8450))*10); % convert to percent
+
+% figure('Position', [500 500 500 500], 'Renderer', 'painters')
+% 
+% plot(albedo_highlands, pmax_highlands, '.', 'Color', [0, 0.4470, 0.7410]); hold on;
+% plot(albedo_maria, pmax_maria, '.', 'Color',  [0.8500, 0.3250, 0.0980]);
+% plot(logA, logPmax, 'k', 'LineWidth', 2)
+% % Create a legend with 3 entries
+% [h,icons] = legend({'highlands', 'maria', 'Umov law'}, 'FontSize', 20);
+% icons = findobj(icons, 'type', 'line');
+% set(icons,'MarkerSize',20);
+% 
+% xlim([0.7 1.6])
+% 
+% xlabel('$\log{A}$ (\%)', 'Interpreter', 'latex')
+% ylabel('$\log{P_{\textrm{max}}}$ (\%)', 'Interpreter', 'latex')
+% 
+% grid on
+% set(gca, 'FontSize', 20)
+
+
+%% Deviation from Umov law
+% figure('Position', [500 500 500 500], 'Renderer', 'painters')
+% 
+% plot(albedo_highlands, pmax_highlands - log10( (10.^((1.81 - albedo_highlands)/0.845))*10), '.', 'Color', [0, 0.4470, 0.7410]); hold on;
+% plot(albedo_maria, pmax_maria - log10( (10.^((1.81 - albedo_maria)/0.845))*10), '.', 'Color',  [0.8500, 0.3250, 0.0980]);
+% % Create a legend with 3 entries
+% % [h,icons] = legend({'highlands', 'maria'}, 'FontSize', 20, 'Location', 'northwest');
+% % icons = findobj(icons, 'type', 'line');
+% % set(icons,'MarkerSize',20);
+% 
+% xlim([0.7 1.6])
+% ylim([-0.4 0.7])
+% 
+% xlabel('$\log{A}$ (\%)', 'Interpreter', 'latex')
+% ylabel('$\Delta\log{P_{\textrm{max}}}$ (\%)', 'Interpreter', 'latex')
+% 
+% grid on
+% set(gca, 'FontSize', 20)
+
+%% 3D histogram
+close all;
+
+maria = [albedo_maria, pmax_maria];
+highlands = [albedo_highlands, pmax_highlands];
+
+% Specify number of bins
+nbins=[200 200];
 
 figure('Position', [500 500 500 500], 'Renderer', 'painters')
-
-plot(albedo_highlands, pmax_highlands, '.', 'Color', [0, 0.4470, 0.7410]); hold on;
-plot(albedo_maria, pmax_maria, '.', 'Color',  [0.8500, 0.3250, 0.0980]);
-plot(logA, logPmax, 'k', 'LineWidth', 2)
-% Create a legend with 3 entries
-[h,icons] = legend({'highlands', 'maria', 'Umov law'}, 'FontSize', 20);
-icons = findobj(icons, 'type', 'line');
-set(icons,'MarkerSize',20);
-
-xlim([0.7 1.6])
-
+[N_highlands, C_highlands] = hist3(highlands, nbins);
+hist3(highlands, nbins, 'CDataMode','auto','FaceColor','interp', 'EdgeColor', 'none');
+view(0,90)
+crameri bilbao
 xlabel('$\log{A}$ (\%)', 'Interpreter', 'latex')
 ylabel('$\log{P_{\textrm{max}}}$ (\%)', 'Interpreter', 'latex')
-
-grid on
 set(gca, 'FontSize', 20)
-
-
+xlim([0.8 1.6])
+ylim([1.5 2.3])
+xl = xlim;
+yl = ylim;
+xticks([0.3 0.5 0.7 0.9 1.1 1.3 1.5 1.7])
+grid off; box on;
+hold on;
+plot3(logA, logPmax, linspace(max(max(N_highlands)),max(max(N_highlands)),length(logA)), '--k')
 
 figure('Position', [500 500 500 500], 'Renderer', 'painters')
-
-plot(albedo_highlands, pmax_highlands - log10( (10.^((1.81 - albedo_highlands)/0.724))*10), '.', 'Color', [0, 0.4470, 0.7410]); hold on;
-plot(albedo_maria, pmax_maria - log10( (10.^((1.81 - albedo_maria)/0.724))*10), '.', 'Color',  [0.8500, 0.3250, 0.0980]);
-% Create a legend with 3 entries
-[h,icons] = legend({'highlands', 'maria'}, 'FontSize', 20);
-icons = findobj(icons, 'type', 'line');
-set(icons,'MarkerSize',20);
-
-xlim([0.7 1.6])
-
+[N_maria, C_maria] = hist3(maria, nbins);
+hist3(maria, nbins, 'CDataMode','auto','FaceColor','interp', 'EdgeColor', 'none');
+view(0,90)
+crameri bilbao
 xlabel('$\log{A}$ (\%)', 'Interpreter', 'latex')
-ylabel('$\Delta\log{P_{\textrm{max}}}$ (\%)', 'Interpreter', 'latex')
-
-grid on
+ylabel('$\log{P_{\textrm{max}}}$ (\%)', 'Interpreter', 'latex')
 set(gca, 'FontSize', 20)
+xlim(xl)
+ylim(yl)
+xticks([0.3 0.5 0.7 0.9 1.1 1.3 1.5 1.7])
+grid off; box on;
+hold on;
+plot3(logA, logPmax, linspace(max(max(N_maria)),max(max(N_maria)),length(logA)), '--k')
 
 
 
