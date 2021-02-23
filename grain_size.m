c2c8c181d4ffebda76d313739b047689df3ddbd2
Aaron Bakx <a.h.j.bakx@student.tudelft.nl>
2021-02-23 15:53:39 +0100

    Self-made regression line and uncertainty analysis

--- grain_size.m
+++ grain_size.m
@@ -11,8 +11,8 @@ fits_albedo_r = load_fits("Ar");
 fits_pmax_v = load_fits("Pv");
 fits_pmax_r = load_fits("Pr");
 
-albedo_b = log10(100*load_fits("Ab"));
-pmax_b = log10(1000*load_fits("Pb"));
+albedo_b = log10(100*load_fits("Ab")); % units of log% percent
+pmax_b = log10(1000*load_fits("Pb")); % units of log%% permilli
 
 %% Interpolate to 630 nm
 
@@ -31,6 +31,94 @@ for i = 1:size_pmax(1)
     end
 end
 
+%% Estimate regression line with least-squares fit
+
+x = -albedo_630(longitude>-100);
+y = pmax_630(longitude>-100);
+
+H = ones(length(x), 2);
+for i = 1:length(x)
+    H(i,1) = x(i);
+end
+
+xhat = inv((transpose(H)*H))*transpose(H)*y;
+yhat = H*xhat;
+
+%plot(-x, yhat)
+
+a = 1/xhat(1);
+b = a * xhat(2);
+
+py = cov(y);
+px = inv(transpose(H)*inv(py)*H);
+
+dx1 = px(1,1);
+da = xhat(1)^2*dx1;
+
+dx2 = px(2,2);
+db = a*xhat(2)*sqrt( (da/a)^2 + (dx2/xhat(2))^2 );
+
+
+
+%% Visualise 630 nm data with regression line
+
+[maria_mask, highlands_mask] = get_maria_and_highlands_mask();
+
+albedo_maria = albedo_630(maria_mask);
+albedo_highlands = albedo_630(highlands_mask);
+
+pmax_maria = pmax_630(maria_mask);
+pmax_highlands = pmax_630(highlands_mask);
+
+maria = [albedo_maria, pmax_maria];
+highlands = [albedo_highlands, pmax_highlands];
+
+
+logA = linspace(0.7, 1.6, 10);
+logPmax = log10( (10.^((1.871 - logA)/0.7950))*10); % convert to percent
+
+
+% Specify number of bins
+nbins=[200 200];
+
+figure('Position', [500 500 500 500], 'Renderer', 'painters')
+[N_highlands, C_highlands] = hist3(highlands, nbins);
+hist3(highlands, nbins, 'CDataMode','auto','FaceColor','interp', 'EdgeColor', 'none');
+view(0,90)
+crameri bilbao
+xlabel('$\log{A}$ (\%)', 'Interpreter', 'latex')
+ylabel('$\log{P_{\textrm{max}}}$ (\%)', 'Interpreter', 'latex')
+set(gca, 'FontSize', 20)
+xlim([0.8 1.6])
+ylim([1.5 2.3])
+xl = xlim;
+yl = ylim;
+xticks([0.3 0.5 0.7 0.9 1.1 1.3 1.5 1.7])
+grid off; box on;
+hold on;
+plot3(logA, logPmax, linspace(max(max(N_highlands)),max(max(N_highlands)),length(logA)),'--k')
+plot3(-x, yhat, linspace(max(max(N_highlands)),max(max(N_highlands)),length(x)), 'k')
+
+
+figure('Position', [500 500 500 500], 'Renderer', 'painters')
+[N_maria, C_maria] = hist3(maria, nbins);
+hist3(maria, nbins, 'CDataMode','auto','FaceColor','interp', 'EdgeColor', 'none');
+view(0,90)
+crameri bilbao
+xlabel('$\log{A}$ (\%)', 'Interpreter', 'latex')
+ylabel('$\log{P_{\textrm{max}}}$ (\%)', 'Interpreter', 'latex')
+set(gca, 'FontSize', 20)
+xlim(xl)
+ylim(yl)
+xticks([0.3 0.5 0.7 0.9 1.1 1.3 1.5 1.7])
+grid off; box on;
+hold on;
+p1 = plot3(logA, logPmax, linspace(max(max(N_maria)),max(max(N_maria)),length(logA)), '--k');
+p2 = plot3(-x, yhat, linspace(max(max(N_maria)),max(max(N_maria)),length(x)), 'k');
+legend([p1 p2],'Shkuratov & Opanasenko (1992)', 'Best LLS fit')
+
+
+
 %% Plot 630 nm maps with maria shape
 
 % plot_map(albedo_630, ' $A$', latitude, longitude, true, 'Albedo650', true)
@@ -38,19 +126,22 @@ end
 
 %% Calculate grain size for 630 nm
 
-a = 0.845;
+a_SO92 = 0.845;
+grain_size_SO92 = get_grain_size(albedo_630, pmax_630, a_SO92);
+
+grain_size_self = get_grain_size(albedo_630, pmax_630, a);
+
+
+%% Plot polarimetric anomaly
 
 size_pixels = size(latitude);
-grain_size_SO92 = zeros(size_pixels);
 polarimetric_anomaly = zeros(size_pixels);
 for i = 1:size_pixels(1)
     for j = 1:size_pixels(2)
         polarimetric_anomaly(i,j) = 10^(pmax_b(i,j)^0.795)*10^albedo_b(i,j); % for B band
-        grain_size_SO92(i,j) = 0.03 * exp( 2.9 * ( albedo_630(i,j) + a * pmax_630(i,j) ) );
     end
 end
 
-%% Plot polarimetric anomaly
 close all;
 figure('Position', [500 500 900 900])
 axesm vperspec
@@ -83,20 +174,7 @@ set(gca, 'FontSize', 20)
 
 %% Visualise Umov's law in 630 nm images
 
-[maria_mask, highlands_mask] = get_maria_and_highlands_mask();
 
-albedo_maria = albedo_630(maria_mask);
-% albedo_maria = albedo_maria(1:1000,:);
-albedo_highlands = albedo_630(highlands_mask);
-% albedo_highlands = albedo_highlands(1:1000,:);
-
-pmax_maria = pmax_630(maria_mask);
-% pmax_maria = pmax_maria(1:1000,:);
-pmax_highlands = pmax_630(highlands_mask);
-% pmax_highlands = pmax_highlands(1:1000,:);
-
-logA = linspace(0.7, 1.6, 10);
-logPmax = log10( (10.^((1.871 - logA)/0.7950))*10); % convert to percent
 
 % figure('Position', [500 500 500 500], 'Renderer', 'painters')
 % 
@@ -136,91 +214,32 @@ logPmax = log10( (10.^((1.871 - logA)/0.7950))*10); % convert to percent
 % grid on
 % set(gca, 'FontSize', 20)
 
-%% 3D histogram
-close all;
-
-maria = [albedo_maria, pmax_maria];
-highlands = [albedo_highlands, pmax_highlands];
-
-% Specify number of bins
-nbins=[200 200];
-
-figure('Position', [500 500 500 500], 'Renderer', 'painters')
-[N_highlands, C_highlands] = hist3(highlands, nbins);
-hist3(highlands, nbins, 'CDataMode','auto','FaceColor','interp', 'EdgeColor', 'none');
-view(0,90)
-crameri bilbao
-xlabel('$\log{A}$ (\%)', 'Interpreter', 'latex')
-ylabel('$\log{P_{\textrm{max}}}$ (\%)', 'Interpreter', 'latex')
-set(gca, 'FontSize', 20)
-xlim([0.8 1.6])
-ylim([1.5 2.3])
-xl = xlim;
-yl = ylim;
-xticks([0.3 0.5 0.7 0.9 1.1 1.3 1.5 1.7])
-grid off; box on;
-hold on;
-plot3(logA, logPmax, linspace(max(max(N_highlands)),max(max(N_highlands)),length(logA)), '--k')
-
-figure('Position', [500 500 500 500], 'Renderer', 'painters')
-[N_maria, C_maria] = hist3(maria, nbins);
-hist3(maria, nbins, 'CDataMode','auto','FaceColor','interp', 'EdgeColor', 'none');
-view(0,90)
-crameri bilbao
-xlabel('$\log{A}$ (\%)', 'Interpreter', 'latex')
-ylabel('$\log{P_{\textrm{max}}}$ (\%)', 'Interpreter', 'latex')
-set(gca, 'FontSize', 20)
-xlim(xl)
-ylim(yl)
-xticks([0.3 0.5 0.7 0.9 1.1 1.3 1.5 1.7])
-grid off; box on;
-hold on;
-plot3(logA, logPmax, linspace(max(max(N_maria)),max(max(N_maria)),length(logA)), '--k')
 
 
 
 %% Compute grain size for different locations
 
 % Apollo 11
-target_longitude = 23.4333;
-target_latitude = 0.6875;
+A11_longitude = 23.4333;
+A11_latitude = 0.6875;
+[g11, dg11] = get_grain_size_at_lat_lon(grain_size_self, a, da, A11_latitude, A11_longitude)
 
-% % Apollo 12
-% target_longitude = -23.3856;
-% target_latitude = -3.1975;
-% 
-% % Apollo 14
-% target_longitude = -17.4653;
-% target_latitude = -3.6733;
+% Apollo 12
+A12_longitude = -23.3856;
+A12_latitude = -3.1975;
+[g12, dg12] = get_grain_size_at_lat_lon(grain_size_self, a, da, A12_latitude, A12_longitude)
 
-% % Apollo 15
-% target_longitude = 3.6527;
-% target_latitude = 26.1008;
+% Apollo 14
+A14_longitude = -17.4653;
+A14_latitude = -3.6733;
+[g14, dg14] = get_grain_size_at_lat_lon(grain_size_self, a, da, A14_latitude, A14_longitude)
 
-% Point of interest
-P = [target_longitude, target_latitude];
+% Apollo 15
+A15_longitude = 3.6527;
+A15_latitude = 26.1008;
+[g15, dg15] = get_grain_size_at_lat_lon(grain_size_self, a, da, A15_latitude, A15_longitude)
 
-% Radius of Moon
-RM = 1737;
 
-% Convert the array of lat/lon coordinates to Cartesian vectors
-% ph2cart expects radians
-% use radius 1, so normalisation of vectors not needed
-[X,Y,Z] = sph2cart( longitude*pi/180,  latitude*pi/180, 1);
-
-% Same for point of interest    
-[xP,yP,zP] = sph2cart(P(1)*pi/180, P(2)*pi/180, 1);
-
-% The minimum distance, and the linear index where that distance was found
-% force the dot product into the interval [-1 +1]. This prevents 
-% slight overshoots due to numerical artifacts
-dotProd = xP*X(:) + yP*Y(:) + zP*Z(:);
-[minDist, index] = min( RM*acos( min(max(-1,dotProd),1) ) );
-
-% Convert that linear index to 2D subscripts
-[lon,lat] = ind2sub(size(longitude), index);
-
-grain_size_SO92(lon,lat);
 
 %% Plot grain size
 
@@ -230,17 +249,17 @@ geoshow(latitude, longitude, grain_size_SO92, 'DisplayType','texturemap')
 
 
 % Apollo landing sites
-plotm(0.6875, 23.4333, 'xw', 'MarkerSize', 20);
-textm(0.6875, 23.4333,'  A11', 'FontSize', 20, 'Color', 'w')
+plotm(A11_latitude, A11_longitude, 'xw', 'MarkerSize', 20);
+textm(A11_latitude, A11_longitude,'  A11', 'FontSize', 20, 'Color', 'w')
 
-plotm(-3.1975, -23.3856, 'xw', 'MarkerSize', 20);
-textm(-3.1975, -23.3856,'A12  ', 'FontSize', 20, 'HorizontalAlignment', 'right', 'Color', 'w')
+plotm(A12_latitude, A12_longitude, 'xw', 'MarkerSize', 20);
+textm(A12_latitude, A12_longitude,'A12  ', 'FontSize', 20, 'HorizontalAlignment', 'right', 'Color', 'w')
 
-plotm(-3.6733, -17.4653, 'xw', 'MarkerSize', 20);
-textm(-3.6733, -17.4653,'  A14', 'FontSize', 20, 'Color', 'w')
+plotm(A14_latitude, A14_longitude, 'xw', 'MarkerSize', 20);
+textm(A14_latitude, A14_longitude,'  A14', 'FontSize', 20, 'Color', 'w')
 
-plotm(26.1008, 3.6527, 'xw', 'MarkerSize', 20);
-textm(26.1008, 3.6527,'  A15', 'FontSize', 20, 'Color', 'w')
+plotm(A15_latitude, A15_longitude, 'xw', 'MarkerSize', 20);
+textm(A15_latitude, A15_longitude,'  A15', 'FontSize', 20, 'Color', 'w')
 
 crameri lajolla
 cbar = colorbar('southoutside');
@@ -268,3 +287,68 @@ axis off
 set(gca, 'FontSize', 20)
 
 % exportgraphics(gca,'Figures/WP3/grain_size.png','Resolution', 300)
+
+%% Functions
+
+function grain_size = get_grain_size(albedo_630, pmax_630, a)
+      
+    % albedo and pmax in natural units
+    
+    latitude = load_fits("latitude");
+    longitude = load_fits("longitude");
+    
+    size_pixels = size(latitude);
+    grain_size = zeros(size_pixels);
+    for i = 1:size_pixels(1)
+        for j = 1:size_pixels(2)
+            grain_size(i,j) = 0.03 * exp( 2.9 * ( albedo_630(i,j) + a * pmax_630(i,j) ) );
+        end
+    end
+
+end
+
+function [g, dg] = get_grain_size_at_lat_lon(grain_size, a, da, target_latitude, target_longitude)
+    
+    latitude = load_fits("latitude");
+    longitude = load_fits("longitude");
+
+    % Point of interest
+    P = [target_longitude, target_latitude];
+
+    % Radius of Moon
+    RM = 1737;
+
+    % Convert the array of lat/lon coordinates to Cartesian vectors
+    % ph2cart expects radians
+    % use radius 1, so normalisation of vectors not needed
+    [X,Y,Z] = sph2cart( longitude*pi/180,  latitude*pi/180, 1);
+
+    % Same for point of interest    
+    [xP,yP,zP] = sph2cart(P(1)*pi/180, P(2)*pi/180, 1);
+
+    % The minimum distance, and the linear index where that distance was found
+    % force the dot product into the interval [-1 +1]. This prevents 
+    % slight overshoots due to numerical artifacts
+    dotProd = xP*X(:) + yP*Y(:) + zP*Z(:);
+    [minDist, index] = min( RM*acos( min(max(-1,dotProd),1) ) );
+
+    % Convert that linear index to 2D subscripts
+    [lon,lat] = ind2sub(size(longitude), index);
+
+    g = grain_size(lon,lat);
+    dg = get_uncertainty(10^(latitude(lon, lat)), 10^(longitude(lon,lat)), g, a, da);
+end
+
+function dd = get_uncertainty(A, Pmax, d, a, da)
+    % A, Pmax in %
+    % d in m
+    dA = 0.8/100*A;
+    dLogA = dA/(log(10)*A);
+
+    dPmax = 0.8/100*Pmax;
+    dLogPmax = dPmax/(log(10)*Pmax);
+    daLogPmax = a * log10(Pmax)*sqrt( (da/a)^2 + (dLogPmax/log10(Pmax))^2);
+
+    dd = d * 2.9 * sqrt( (dLogA)^2 + (daLogPmax)^2);
+
+end
