6f7f4d8496ff9e3f25f51d0a64ace058ae10d0c9
Aaron Bakx <a.h.j.bakx@student.tudelft.nl>
2021-03-03 11:27:11 +0100

    Local effective density spectrum

--- localisation.py
+++ localisation.py
@@ -2,6 +2,13 @@
 Created on Tue Mar  2 10:15:14 2021
 
 @author: aaron
+
+Localised spectral analysis - multitaper approach
+
+    * spherical caps of given radius and bandwith
+    * local effective density spectrum
+    * linear least-squares fit of compaction models
+
 """
 
 import pyshtools as pysh
@@ -19,22 +26,24 @@ pysh.utils.figstyle(rel_width=0.5)
 # Maximum degree
 lmin=250
 lmax=650
+lwin = 58
+
 
 # clm = pysh.datasets.Moon.GRGM1200B_RM1_1E0(lmax=lmax)
 clm = pysh.SHGravCoeffs.from_file('/Users/aaron/thesis/Data/moon_gravity/sha.grgm1200b_rm1_1e1_sigma.txt',
                                   r0_index=1,
                                   gm_index=0,
-                                  errors=True)
+                                  errors=True, lmax=lmax+lwin)
 
-hlm = pysh.datasets.Moon.MoonTopo2600p(lmax=1200)
+hlm = pysh.datasets.Moon.MoonTopo2600p(lmax=lmax+lwin)
 
 # set angular rotation rate (for centripetal force)
 clm.set_omega(constants.Moon.omega.value) 
 
-spectrum = clm.spectrum(function='total', lmax=1200)
-degrees = np.arange(lmin, lmax+1)
+spectrum = clm.spectrum(function='total', lmax=lmax+lwin)
+degrees = np.arange(lmax+lwin+1)
 
-bc = pysh.SHGravCoeffs.from_shape(hlm, rho=1., gm=clm.gm, lmax=1200)
+bc = pysh.SHGravCoeffs.from_shape(hlm, rho=1., gm=clm.gm, lmax=lmax+lwin)
 bc = bc.change_ref(r0=clm.r0)
 
 ghat = pysh.SHCoeffs.from_array(bc.coeffs)
@@ -42,92 +51,70 @@ gobs = pysh.SHCoeffs.from_array(clm.coeffs)
 
 global_eff_dens = gobs.admittance(ghat)
 
-global_grid = pysh.SHCoeffs.from_array(clm.coeffs).expand()
 
-#%% Multitaper approach
+#%% Define multitaper spherical caps
 
-lwin = 58
+clat = 0.
+clon = 270.
 caprad = 15.
+concentration_threshold = 0.99
 
+# Construct spherical caps with certain radius and bandwith
 capwin = pysh.SHWindow.from_cap(theta=caprad, lwin=lwin)
 
-k = capwin.number_concentrated(0.99)
+# Use the best caps above a concentration threshold
+k = capwin.number_concentrated(concentration_threshold)
 
-clat = 70.
-clon = 270.
+# Rotate best spherical caps to lonlat of interest
 capwin.rotate(clat=clat, clon=clon, nwinrot=k)
 
-mts_topo, mt_topo_sd = capwin.multitaper_spectrum(hlm, k,
+# Determine multitaper (local) spectrum of Bouguer correction
+mts_bc, mt_bc_sd = capwin.multitaper_spectrum(ghat, k,
                                                     clat=clat,
                                                     clon=clon)
 
-mts_topograv, mt_topograv_sd = capwin.multitaper_cross_spectrum(bc, hlm, k,
+# Determine multitaper (local) cross spectrum of gravity and Bouguer correction
+mtxs_topograv, mt_topograv_sd = capwin.multitaper_cross_spectrum(ghat, gobs, k,
                                                     clat=clat,
                                                     clon=clon)
 
-# local_spectrum = capwin.biased_spectrum(spectrum[0], k)
-# local_eff_dens = local_topograv_spectrum / local_topography_spectrum
+# Calculate local effective density
+local_eff_dens = mtxs_topograv / mts_bc
 
+#%% Least squares fit of local spectrum
+
+k = np.sqrt(degrees[lmin:lmax+1]*(degrees[lmin:lmax+1]+1))/(clm.r0/1000)
+x = 1/k
+y = local_eff_dens[lmin:lmax+1]
+
+H = np.ones((len(x), 2));
+for i in range(len(x)):
+    H[i,0] = x[i]
+
+xhat = np.matmul(np.matmul(np.linalg.inv(np.matmul(np.transpose(H),H)), np.transpose(H)),y)
+yhat = np.matmul(H, xhat)
+
+Px = np.linalg.inv( 1/np.cov(y)*np.matmul(np.transpose(H), H) )
+
+print('Initial guess:\n', 'a=', xhat[0], '+-', np.sqrt(Px[0,0]), '\n', 
+      'rho=', xhat[1], '+-', np.sqrt(Px[1,1]), '\n',
+      'effective density +- is', np.sqrt(Px[0,0] + Px[1,1]))
+
+eff_dens_th = xhat[1] + xhat[0]/k
 
 #%% Plot local spectrum
 
 capwin.plot_windows(1, loss=True, show=False)
+ctud=[0,0.65,0.84]
 
 fig, ax = plt.subplots(1,1)
-ax.plot(np.arange(1143), mts_topo)
-ax.plot(np.arange(1201), hlm.spectrum(lmax=1200))
-ax.set(yscale='log', xlim=(lmin, lmax))
-
-bcx = pysh.SHCoeffs.from_array(bc.coeffs)
-fig, ax = plt.subplots(1,1)
-ax.plot(np.arange(1143), mts_topograv)
-ax.plot(np.arange(1201), hlm.cross_spectrum(bcx,lmax=1200))
-ax.set(yscale='log', xlim=(lmin, lmax))
-
-
-
-# fig, ax = plt.subplots(1,1)
-# ax.plot(degrees, spectrum[0][lmin:lmax+1], '-k', label='global')
-# ax.plot(degrees, local_spectrum[lmin:lmax+1], '-b', label='local')
-# ax.set(yscale='log', xlabel='Spherical harmonic degree', ylabel='Power', xlim=(lmin,lmax))
-# ax.legend()
-
-# ctud=[0,0.65,0.84]
-# eff_dens_th = xhat[1] + xhat[0]/k
-# fig, ax = plt.subplots(1,1)
-# ax.plot(degrees, eff_dens[lmin:lmax+1,0], '-k', label='global', linewidth=0.5, linestyle='dotted')
-# ax.plot(degrees, local_eff_dens[lmin:lmax+1], '-k', label='local', linewidth=1)
-# ax.plot(degrees, eff_dens_th, label='theoretical', color=ctud)
-# ax.set(xlabel='Spherical harmonic degree', ylabel='Effective density, kg/m$^3$', xlim=(lmin,lmax))
-# ax.legend()
-# ax.grid()
-
-#%% Determine lons&lats of tapers
-
-def find_nearest(array, value):
-    array = np.asarray(array)
-    idx = (np.abs(array - value)).argmin()
-    return array[idx]
-
-
-grid = capwin.to_shgrid(itaper=4)
-
-array = grid.to_array()
-
-lons = np.linspace(0,360,119)
-lats = np.linspace(90, -90, 119)
-lon, lat = np.meshgrid(lons, lats)
-
-test = find_nearest(lats, clat)
-roilats = (lats > clat - caprad) & (lats < clat + caprad)
-roilons  = (lons > clon - caprad) & (lons < clon + caprad)
-roilon, roilat = np.meshgrid(roilons, roilats)
-
-plt.figure(figsize=(3,3))       
-plt.contourf(lon, lat, roilon&roilat,
-              cmap = scm.sequential.Bilbao_20.mpl_colormap) 
-plt.colorbar()
-
+ax.plot(degrees[lmin:lmax+1], global_eff_dens[lmin:lmax+1,0], '-k', label='global', linewidth=0.5, linestyle='dotted')
+ax.plot(degrees[lmin:lmax+1], local_eff_dens[lmin:lmax+1], '-k', label='local', linewidth=1)
+ax.plot(degrees[lmin:lmax+1], eff_dens_th, label='theoretical fit', color=ctud)
+ax.set(xlabel='Spherical harmonic degree', ylabel='Effective density, kg/m$^3$', 
+       xlim=(lmin,lmax), ylim=(2200, 2600))
+ax.legend()
+ax.grid()
 
 
 
